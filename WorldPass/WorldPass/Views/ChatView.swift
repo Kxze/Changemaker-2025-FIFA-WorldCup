6389daa6eadb9de92ac91df2a86eae2cec1832e9
alvizopaulina22-coder <alvizopaulina22@gmail.com>
2025-10-21 16:38:19 -0600

    Diseño IA Predicciones

---
+++ WorldPass/WorldPass/Views/ChatView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatView.swift
+//  WorldPass
+//
+//  Created by Pau Pau on 21/10/25.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatView()
+}

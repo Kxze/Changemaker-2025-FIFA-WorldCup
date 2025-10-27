f7277d380433fb8969be12fcbe666ae8d01cf3cb
alvizopaulina22-coder <alvizopaulina22@gmail.com>
2025-10-26 22:08:18 -0600

    Dividir gastos funcional. Falta diseño

---
+++ WorldPass/WorldPass/Views/DividirGastosView.swift
@@ -0,0 +1,18 @@
+//
+//  DividirGastosView.swift
+//  WorldPass
+//
+//  Created by Pau Pau on 26/10/25.
+//
+
+import SwiftUI
+
+struct DividirGastosView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DividirGastosView()
+}

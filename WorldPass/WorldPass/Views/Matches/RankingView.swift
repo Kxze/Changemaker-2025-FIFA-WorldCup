6d0a19dac02b23045c895b0bf5f11334400f6751
alvizopaulina22-coder <alvizopaulina22@gmail.com>
2025-10-26 21:30:16 -0600

    Aimaciones de picknDream y pantallas de ranking

---
+++ WorldPass/WorldPass/Views/Matches/RankingView.swift
@@ -0,0 +1,18 @@
+//
+//  RankingView.swift
+//  WorldPass
+//
+//  Created by Pau Pau on 26/10/25.
+//
+
+import SwiftUI
+
+struct RankingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RankingView()
+}
